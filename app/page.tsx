31c958145bea7d0a2fd5bb9e8fbadf517ca32700
Bipin Koirala <bipinkoirala2061@gmail.com>
2026-02-15 10:19:55 +0545

    Color and Font setup Initialized

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black"></div>
+    <div className="flex gap-4 min-h-screen items-center justify-center bg-background text-text">
+      <div className="flex px-8 py-4 gap-4 bg-foreground rounded-xl hover:brightness-95 cursor-pointer smooth-transition">
+        Is this good or not?
+      </div>
+      <div className="header-font">Different font</div>
+    </div>
   );
 }
