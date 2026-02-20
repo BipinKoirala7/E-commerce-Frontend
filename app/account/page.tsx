3b3a3a1cd9b47c6e2b48583538ee56b78063df59
Bipin Koirala <bipinkoirala2061@gmail.com>
2026-02-20 10:07:35 +0545

    Account page initialized

---
+++ app/account/page.tsx
@@ -0,0 +1,7 @@
+export default function Account() {
+  return (
+    <div className="min-w-[33%] h-full bg-foreground mt-8 mb-8 flex flex-col gap-4 items-center justify-center p-8 rounded-2xl">
+      <p className="header-font text-6xl">Account</p>
+    </div>
+  );
+}
