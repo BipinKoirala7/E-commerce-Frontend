4ffe2135d2f7d09b9534ce56ba33de77f26f5257
Bipin Koirala <bipinkoirala2061@gmail.com>
2026-03-06 09:15:22 +0545

    Women collection tab created

--- components/Home/MensCollection.tsx
+++ components/Home/MensCollection.tsx
@@ -1,11 +1,11 @@
-import MensCollectionShowCaseProductList from "./MensCollectionShowCaseProductList";
+import MensCollectionShowCase from "./MensCollectionShowCase";
 
 function MensCollection() {
   return (
     <div className="min-h-100 flex flex-col gap-4 p-6 bg-primary rounded-4xl">
       <p className="text-3xl header-font font-bold">Mens Collection</p>
       <div className="w-full h-full flex">
-        <MensCollectionShowCaseProductList />
+        <MensCollectionShowCase />
       </div>
     </div>
   );
--- components/Home/WomenCollection.tsx
+++ components/Home/WomenCollection.tsx
@@ -1,5 +1,14 @@
+import WomenCollectionShowCase from "./WomenCollectionShowCase";
+
 function WomenCollection() {
-  return <div>WomenCollection</div>;
+  return (
+    <div className="min-h-100 flex flex-col gap-4 p-6 bg-primary rounded-4xl">
+      <p className="text-3xl header-font font-bold">Womens Collection</p>
+      <div className="w-full h-full flex">
+        <WomenCollectionShowCase />
+      </div>
+    </div>
+  );
 }
 
 export default WomenCollection;
